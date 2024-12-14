a161f866d6f628f062a38100e78eedcd927d2f51
SRIRAM_PALANISAMY <118471711+SRIRAMPRO@users.noreply.github.com>
2024-12-14 22:02:55 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -177,7 +177,7 @@ def main():
         layout="wide",
         initial_sidebar_state="expanded",
     )
-    with open("../assets/style.css") as f:
+    with open("assets/style.css") as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
     input_data=add_sidebar()
